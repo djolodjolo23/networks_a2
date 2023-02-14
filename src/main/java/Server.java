817b83e9cf07a7b2000585ee8a2157c3a158b0db
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-02-14 18:24:05 +0100

    Routing works fine for now, /stuff.html returns the picture

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -32,25 +32,55 @@ public class Server {
 
           String line = bufferedReader.readLine(); // holds one line at the time
           while (!line.isBlank()) {
-            requestBuilder.append(line);
+            requestBuilder.append(line).append("\n");
             line = bufferedReader.readLine();
           }
-          System.out.println("--- REQUEST ---");
-          System.out.println(requestBuilder);
+          //System.out.println("--- REQUEST ---");
+          //System.out.println(requestBuilder);
 
-          /**
-          OutputStream clientOutput = client.getOutputStream();
-          clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
-          clientOutput.write(("\r\n").getBytes());
-          clientOutput.write(("Hello world!").getBytes());
-          clientOutput.flush();
-          */
+          // TODO: How to respond?
+          // Get the first line of the request
+          // Get the second thing 'resource' from the first line
+          // Compare the 'resource' to ouyr list of things
+          // send back the appropriate thing based on the resource
+
+          // Get the first line of the request
+          String firstline =  requestBuilder.toString().split("\n")[0];
+          // Get the second thing 'resource' from the first line
+          String resource = firstline.split(" ")[1];
+          if (resource.equals("/stuff.html")) {
+            FileInputStream image = new FileInputStream("joke.png");
+            System.out.println(image);
+            OutputStream clientOutput = client.getOutputStream();
+            clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+            clientOutput.write(("\r\n").getBytes());
+            clientOutput.write(image.readAllBytes());
+            clientOutput.flush();
+
+          } else if (resource.equals("/hello")) {
+            OutputStream clientOutput = client.getOutputStream();
+            clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+            clientOutput.write(("\r\n").getBytes());
+            clientOutput.write(("Hello world!").getBytes());
+            clientOutput.flush();
+          } else {
+            OutputStream clientOutput = client.getOutputStream();
+            clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+            clientOutput.write(("\r\n").getBytes());
+            clientOutput.write(("What are you looking for???????").getBytes());
+            clientOutput.flush();
+          }
+         //System.out.println(resource);
+
+
+
+
 
           // send back an image???
           // Load the image from the file system
           // turn the image into bytes???
           // Set the ContentType?
-
+          /**
           FileInputStream image = new FileInputStream("joke.png");
           System.out.println(image);
 
@@ -59,6 +89,7 @@ public class Server {
           clientOutput.write(("\r\n").getBytes());
           clientOutput.write(image.readAllBytes());
           clientOutput.flush();
+           */
 
 
 
