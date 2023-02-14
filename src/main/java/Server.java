ae55572f06d281cb915c9cbddf0ae59a65687b15
Djordje Dimitrov <110050027+djolodjolo23@users.noreply.github.com>
2023-02-14 18:01:48 +0100

    Images are returned to the client on the localhost:8888

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,18 +14,20 @@ public class Server {
   public static void main(String[] args) throws IOException {
 
     try (ServerSocket serverSocket = new ServerSocket(port)) {
+      // ready to receive messages
       System.out.println("Server has started.\nListening on port " + port);
       while (true) {
         // client that's accepted
         try (Socket client = serverSocket.accept()) {
-          // simple debug message
+          // client and messages queued up
           System.out.println("Debug message: got a new message " + client.toString());
 
+          // read the requests and listen to the message
           InputStreamReader inputStreamReader = new InputStreamReader(client.getInputStream());
 
           BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-          //
+          // read the first request from the client
           StringBuilder requestBuilder = new StringBuilder();
 
           String line = bufferedReader.readLine(); // holds one line at the time
@@ -35,10 +38,26 @@ public class Server {
           System.out.println("--- REQUEST ---");
           System.out.println(requestBuilder);
 
+          /**
           OutputStream clientOutput = client.getOutputStream();
           clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
           clientOutput.write(("\r\n").getBytes());
           clientOutput.write(("Hello world!").getBytes());
+          clientOutput.flush();
+          */
+
+          // send back an image???
+          // Load the image from the file system
+          // turn the image into bytes???
+          // Set the ContentType?
+
+          FileInputStream image = new FileInputStream("joke.png");
+          System.out.println(image);
+
+          OutputStream clientOutput = client.getOutputStream();
+          clientOutput.write(("HTTP/1.1 200 OK\r\n").getBytes());
+          clientOutput.write(("\r\n").getBytes());
+          clientOutput.write(image.readAllBytes());
           clientOutput.flush();
 
 
